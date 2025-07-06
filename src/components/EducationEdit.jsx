76a70d7889fab4c2b6b5627ec0d648b9c1601178
Mohammadrasool Vahid <121235436+mutopia7@users.noreply.github.com>
2025-07-06 17:51:16 +0330

    Edit: edit style

--- src/components/EducationEdit.jsx
+++ src/components/EducationEdit.jsx
@@ -1,74 +1,92 @@
 import styles from '../styles/EducationEdit.module.css'
+import { useState } from 'react';
 import { FaGraduationCap } from 'react-icons/fa';
+import { FaChevronDown } from 'react-icons/fa'
 
-function EducationEdit({educationData, onChange}){
+function EducationEdit({ educationData, onChange }) {
+
+    // for open and close form
+    const [isOpen, setIsOpen] = useState(true)
+
+    function toggleOpen(){
+        setIsOpen(prev => !prev)
+    }
+
     return (
         <section className={styles.editSec}>
             <h2 className={styles.formHeader}>
-                <FaGraduationCap aria-hidden="true" />
-                <span>Education</span>
+                <div className={styles.formHeaderTitle}>
+                    <FaGraduationCap  aria-hidden="true" />
+                    <span>Education</span>
+                </div>
+
+                <FaChevronDown className={`${styles.arrow} ${isOpen ? styles.rotate : ''}`} onClick={toggleOpen} />
             </h2>
-            <form action="" className={styles.formSec}>
+
+            {isOpen && (
+                <form action="" className={styles.formSec}>
                 <label htmlFor="school">
                     School:
-                    <input 
-                    type="text"
-                    name='school'
-                    id='school'
-                    value={educationData.school}
-                    placeholder='Enter school/ university'
-                    onChange={onChange}
+                    <input
+                        type="text"
+                        name='school'
+                        id='school'
+                        value={educationData.school}
+                        placeholder='Enter school/ university'
+                        onChange={onChange}
                     />
                 </label>
 
                 <label htmlFor="degree">
                     Degree:
-                    <input 
-                    type="text"
-                    name='degree'
-                    id='degree'
-                    value={educationData.degree}
-                    placeholder='Enter Degree/ field of study'
-                    onChange={onChange}
+                    <input
+                        type="text"
+                        name='degree'
+                        id='degree'
+                        value={educationData.degree}
+                        placeholder='Enter Degree/ field of study'
+                        onChange={onChange}
                     />
                 </label>
 
                 <label htmlFor="eduStartDate">
                     Start Date:
-                    <input 
-                    type="month"
-                    name='eduStartDate'
-                    id='eduStartDate'
-                    value={educationData.eduStartDate}
-                    placeholder='Enter start date'
-                    onChange={onChange}
+                    <input
+                        type="month"
+                        name='eduStartDate'
+                        id='eduStartDate'
+                        value={educationData.eduStartDate}
+                        placeholder='Enter start date'
+                        onChange={onChange}
                     />
                 </label>
 
                 <label htmlFor="EduEndDate">
                     End Date:
-                    <input 
-                    type="month"
-                    name='eduEndDate'
-                    id='eduEndDate'
-                    value={educationData.eduEndDate}
-                    placeholder='Enter End date'
-                    onChange={onChange}
+                    <input
+                        type="month"
+                        name='eduEndDate'
+                        id='eduEndDate'
+                        value={educationData.eduEndDate}
+                        placeholder='Enter End date'
+                        onChange={onChange}
                     />
                 </label>
 
                 <label htmlFor="location">
                     Location:
-                    <input 
-                    type='text'
-                    name='location'
-                    id='location'
-                    value={educationData.location}
-                    placeholder='Enter Location'
-                    onChange={onChange}
-                     />
+                    <input
+                        type='text'
+                        name='location'
+                        id='location'
+                        value={educationData.location}
+                        placeholder='Enter Location'
+                        onChange={onChange}
+                    />
                 </label>
             </form>
+            )}
+            
         </section>
     )
 }
