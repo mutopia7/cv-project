ed1f680875d1e25f21c2c493df60999fd692551d
Mohammadrasool Vahid <121235436+mutopia7@users.noreply.github.com>
2025-07-07 14:19:24 +0330

    Add ProfessionalEdit componenet and css file for that

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,67 @@ import PersonalEdit from './components/PersonalEdit';
 import EducationPreview from './components/EducationPreview';
 import EducationEdit from './components/EducationEdit';
 import ProfessionalPreview from './components/ProfessionalPreview';
+import ProfessionalEdit from './components/ProfessionalEdit';
 import './App.css';
+
+
+const personalInf = {
+    name: 'Rasool Vahid',
+    email: 'Mutopia20@gmail.com',
+    phone: '+98 902 9292 ***',
+    address: 'Rasht, IRI',
+};
+
+const eduList = [
+    {
+        id: 1,
+        school: 'Odin Project',
+        degree: 'Bachelors in FullStack',
+        eduStartDate: '2025-03',
+        eduEndDate: '2025-08',
+        location: 'Online',
+        isOpen: false,
+    },
+    {
+        id: 2,
+        school: 'MIT',
+        degree: 'Masters in AI',
+        eduStartDate: '2024-01',
+        eduEndDate: '2024-12',
+        location: 'Cambridge',
+        isOpen: false,
+    }
+];
+
+const proList = [
+    {
+        id: 1,
+        company: 'Freelance Developer',
+        position: 'My own employer',
+        proStartDate: '2023-01',
+        proEndDate: '2025-05',
+        location: 'Rasht, IRI ',
+        description: 'worked on various projects ranging from web development to game development'
+
+    },
+    {
+        id: 2,
+        company: 'Sample Inc.',
+        position: 'My own employer',
+        proStartDate: '2022-01',
+        proEndDate: '2023-01',
+        location: 'Rasht, IRI ',
+        description: 'Designing multiple websites in a responsive manner. Designing multiple websites in a responsive manner. Designing multiple websites in a responsive manner. Designing multiple websites in a responsive manner. Designing multiple websites in a responsive manner.'
+
+    }
+];
+
 
 
 function App() {
 
     // state for personal data
-    const [formData, setFormData] = useState({
-        name: 'Rasool Vahid',
-        email: 'Mutopia20@gmail.com',
-        phone: '+98 902 9292 ***',
-        address: 'Rasht, IRI',
-    });
+    const [formData, setFormData] = useState(personalInf);
 
     // handle change for inputs within personal form
     function handleChange(e) {
@@ -28,26 +77,7 @@ function App() {
 
 
     // state for education data
-    const [educationList, setEducationList] = useState([
-        {
-            id: 1,
-            school: 'Odin Project',
-            degree: 'Bachelors in FullStack',
-            eduStartDate: '2025-03',
-            eduEndDate: '2025-08',
-            location: 'Online',
-            isOpen: false,
-        },
-        {
-            id: 2,
-            school: 'MIT',
-            degree: 'Masters in AI',
-            eduStartDate: '2024-01',
-            eduEndDate: '2024-12',
-            location: 'Cambridge',
-            isOpen: false,
-        }
-    ]);
+    const [educationList, setEducationList] = useState(eduList);
 
     // handle chane for inputs within education form
     function handleEducationChange(e, id) {
@@ -60,40 +90,20 @@ function App() {
     }
 
     // state for professional data
-    const [professionalList, setProfessionalList] = useState([
-        {
-            id: 1,
-            company: 'Freelance Developer',
-            position: 'My own employer',
-            proStartDate: '2023-01',
-            proEndDate: '2025-05',
-            location: 'Rasht, IRI ',
-            description: 'worked on various projects ranging from web development to game development'
-
-        },
-        {
-            id: 2,
-            company: 'Sample Inc.',
-            position: 'My own employer',
-            proStartDate: '2022-01',
-            proEndDate: '2023-01',
-            location: 'Rasht, IRI ',
-            description: 'Designing multiple websites in a responsive manner. Designing multiple websites in a responsive manner. Designing multiple websites in a responsive manner. Designing multiple websites in a responsive manner. Designing multiple websites in a responsive manner.'
-
-        }
-    ]);
+    const [professionalList, setProfessionalList] = useState(proList);
 
     return (
         <main className='app'>
             <section className='editor'>
                 <PersonalEdit formData={formData} onChange={handleChange} />
                 <EducationEdit educationList={educationList} setEducationList={setEducationList} onChange={handleEducationChange} />
-                
+                <ProfessionalEdit />
+
             </section>
             <section className='preview'>
                 <Header {...formData} />
                 <EducationPreview educationList={educationList} />
-                <ProfessionalPreview professionalList={professionalList}/>
+                <ProfessionalPreview professionalList={professionalList} />
             </section>
 
         </main>
---
+++ src/components/ProfessionalEdit.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/ProfessionalEdit.module.css'
+
+
+function ProfessionalEdit(){
+    return (
+        <div className={styles.l}>sdfsf</div>
+    )
+}
+
+
+export default ProfessionalEdit;
